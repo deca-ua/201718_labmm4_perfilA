b4cf6af3de59a414e45375fe859228da0f03e350
Pedro Beça <PedroBeca@ua.pt>
2018-05-03 11:09:38 +0100

    Exercício prático PHP_MYSQL_ex_01

---
+++ originais/p_18_PHP_MYSQL_ex_01/components/team_members_db.php
@@ -0,0 +1,18 @@
+<div class="row">
+    <div class="col-lg-12">
+        <h2 class="page-header">A nossa equipa <a href="?sort="><i class="fa fa-sort-alpha-down fa-xs"
+                                                                   aria-hidden="true"></i></a></h2>
+    </div>
+
+
+    <div class="col-lg-4 col-sm-6 text-center">
+        <a href="member_info.php?id={id_mentors}">
+            <img class="img-circle img-responsive img-center" src="images/{image}" alt="">
+        </a>
+        <h3>{name}
+            <small>{title}</small>
+        </h3>
+        <p>{description_short}</p>
+    </div>
+
+</div>
---
+++ originais/p_18_PHP_MYSQL_ex_01/helpers/css.php
@@ -0,0 +1,7 @@
+<!-- Bootstrap Core CSS -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/web-fonts-with-css/css/fontawesome-all.min.css" rel="stylesheet">
+
+<!-- Custom CSS -->
+<link href="css/round-about.css" rel="stylesheet">
+<link href="css/custom.css" rel="stylesheet">
