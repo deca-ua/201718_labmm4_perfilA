d0a82fc9c40c172279f249d2afc6de821a3b3e1b
Pedro Beça <PedroBeca@ua.pt>
2018-05-03 11:32:32 +0100

    Exercício prático PHP_MYSQL_ex_01

--- originais/p_18_PHP_MYSQL_ex_01/helpers/css.php
+++
@@ -1,7 +0,0 @@
-<!-- Bootstrap Core CSS -->
-<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-<link href="css/web-fonts-with-css/css/fontawesome-all.min.css" rel="stylesheet">
-
-<!-- Custom CSS -->
-<link href="css/round-about.css" rel="stylesheet">
-<link href="css/custom.css" rel="stylesheet">
