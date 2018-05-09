0764d76d5181633bc52070f89e126da6381b08ec
Pedro Beça <PedroBeca@ua.pt>
2018-05-09 21:47:10 +0100

    Exercício prático PHP_MYSQL_ex_03a

--- originais/p_20_PHP_MySQL_ex_03a/admin/index.php
+++ originais/p_20_PHP_MySQL_ex_03a/admin/index.php
@@ -6,6 +6,6 @@
 </head>
 <body>
   <h1>Bem-vindo à ferramenta de administração</h1>
-  <a href="../public/index.php">voltar àrea pública</a>
+  <a href="../public/index.php">voltar à área pública</a>
 </body>
 </html>
